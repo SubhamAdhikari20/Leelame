c499720d896a91920e4b30d77de23ab10dd06a68
Subham Adhikari <subhamadhikari20@gmail.com>
2025-03-09 23:12:06 +0545

    Initiazation of Leelame - an online auction platform

---
+++ src/app/api/sign-up/route.ts
@@ -0,0 +1,126 @@
+import databaseConnection from "@/lib/databseConnection";
+import UserModel from "@/model/UserModel";
+import bcrypt from "bcryptjs";
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+
+
+export const POST = async (req: Request) => {
+    await databaseConnection();
+
+    try {
+        const { fullName, username, email, password, contact } = await req.json();
+
+        const existingUserVerifiedByUsername = await UserModel.findOne({
+            username,
+            isVerified: false
+        });
+
+        if (existingUserVerifiedByUsername) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "Username is already taken"
+                },
+                {
+                    status: 400
+                }
+            );
+        }
+
+        const existingUserByEmail = await UserModel.findOne({ email });
+
+        const otp = Math.floor(100000 + Math.random() * 900000).toString();
+
+        if (existingUserByEmail) {
+            if (existingUserByEmail.isVerified) {
+                return Response.json(
+                    {
+                        success: false,
+                        message: "User already exists with this email"
+                    },
+                    {
+                        status: 400
+                    }
+                );
+            }
+            else {
+                const salt = bcrypt.genSaltSync(10);
+                const hashedPassword = await bcrypt.hash(password, salt);
+                const expiryDate = new Date()
+                expiryDate.setHours(expiryDate.getHours() + 1);
+
+                existingUserByEmail.password = hashedPassword;
+                existingUserByEmail.verifyCode = otp;
+                existingUserByEmail.verifyCodeExpiryDate = expiryDate;
+                await existingUserByEmail.save();
+            }
+            return Response.json(
+                {
+                    success: false,
+                    message: "Email is already taken"
+                },
+                {
+                    status: 400
+                }
+            );
+        }
+        else {
+            const salt = bcrypt.genSaltSync(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+
+            const expiryDate = new Date()
+            expiryDate.setHours(expiryDate.getHours() + 1);
+
+            const newUser = new UserModel({
+                fullName,
+                username,
+                email,
+                password: hashedPassword,
+                contact,
+                verifyCode: otp,
+                verifyCodeExpiryDate: expiryDate,
+                isVerified: false
+            });
+
+            await newUser.save();
+
+        }
+
+        // send verfication email
+        const emailResponse = await sendVerificationEmail(fullName, email, otp);
+
+        if (!emailResponse.success) {
+            return Response.json(
+                {
+                    success: false,
+                    message: emailResponse.message
+                },
+                {
+                    status: 500
+                }
+            );
+        }
+
+        return Response.json(
+            {
+                success: true,
+                message: "User signed up successfully. Please verify your email"
+            },
+            {
+                status: 201
+            }
+        );
+    }
+    catch (error) {
+        console.error("Error signing up the user: ", error);
+        return Response.json(
+            {
+                success: false,
+                message: "Error signing up the user"
+            },
+            {
+                status: 500
+            }
+        );
+    }
+};
